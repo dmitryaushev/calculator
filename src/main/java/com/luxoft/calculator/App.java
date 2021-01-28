e353db2f959e24c20cc228a5d4efc25174634c81
Aushev <DAushev@luxoft.com>
2021-01-28 10:27:08 +0200

    Updated redundant files

--- src/main/java/com/luxoft/calculator/App.java
+++ src/main/java/com/luxoft/calculator/App.java
@@ -1,7 +1,13 @@
 package com.luxoft.calculator;
 
+import org.eclipse.swt.widgets.Display;
+
+import com.luxoft.calculator.ui.Gui;
+
 public class App {
 	public static void main(String[] args) {
-		new Gui().createGui();
+		Display display = new Display();
+		new Gui().createGui(display);
+		display.dispose();
 	}
 }
